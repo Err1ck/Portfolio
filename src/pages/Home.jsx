76019ffe613cbacfd4ec21625de4cf7d5a0e4054
Err1ck <erryck-1996@hotmail.com>
2025-05-12 16:22:04 +0200

    refactor:SkillsSection Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { StarBackground } from "@/components/StarBackground";
 import { Navbar } from "@/components/Navbar";
 import { MainSection } from "@/components/MainSection";
 import { AboutSection } from "@/components/AboutSection";
-import { SkillsSection } from "@/components/SkillsSeccion";
+import { SkillsSection } from "@/components/SkillsSection";
 import { ProjectsSection } from "@/components/ProjectsSection";
 import { ContactSection } from "@/components/ContactSection";
 import { Footer } from "@/components/Footer";
